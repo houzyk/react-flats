c6f13f5ac22149add75c1372d053a2da7462398b
houzairkoussa <houzairmk@icloud.com>
2021-12-21 11:59:10 +0400

    INIT google map react

--- src/components/Googlemap.jsx
+++ src/components/Googlemap.jsx
@@ -1,8 +1,30 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
 
-function Googlemap () {
+// Components
+import Marker from './Marker';
+
+function Googlemap ({ lat, lng }) {
+  const defaultProps = {
+    center: {
+      lat: lat,
+      lng: lng
+    },
+    zoom: 11
+  };
+
   return (
     <div className="map-container">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "" }}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <Marker
+          lat={lat}
+          lng={lng}
+        />
+      </GoogleMapReact>
     </div>
   );
 }
--- src/components/Marker.jsx
+++ src/components/Marker.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Marker () {
-  return (<></>);
+  return <div className="marker" />;
 }
 
 export default Marker;
